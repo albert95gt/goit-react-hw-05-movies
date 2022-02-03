118153995caffe9b2488edf2ceae512d6ec56930
Albert Takács <albitakacs@gmail.com>
2022-02-03 17:16:29 +0100

    add setup files

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+export const App = () => {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        textTransform: 'uppercase',
+        color: '#010101',
+      }}
+    >
+      React homework template
+    </div>
+  );
+};
