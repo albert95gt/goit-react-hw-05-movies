51f55876cc15ca478cbd1f7afefadf95d2858f3f
albert95gt <84626708+albert95gt@users.noreply.github.com>
2022-02-11 20:55:35 +0000

    Deploying to gh-pages from @ albert95gt/goit-react-hw-05-movies@bfb4cc47e34ae5dff7b7d3be0d5016b7af778190 🚀

---
+++ static/js/736.134d945f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[736],{2736:function(e,r,n){n.r(r),n.d(r,{Reviews:function(){return c}});var t=n(184),c=function(e){var r=e.reviews;return(0,t.jsx)("ul",{children:r.map((function(e){var r=e.id,n=e.author,c=e.content;return(0,t.jsxs)("li",{children:[(0,t.jsxs)("h3",{children:["Author: ",n]}),(0,t.jsx)("p",{children:c})]},r)}))})}}}]);
+//# sourceMappingURL=736.134d945f.chunk.js.map
